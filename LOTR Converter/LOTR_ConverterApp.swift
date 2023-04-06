ed6bdc050e2f8d7fe12603c6c81b1ee251aa0d91
Marcus Kim <marcuskim1989@gmail.com>
2023-04-05 18:28:14 -0700

    Initial Commit

---
+++ LOTR Converter/LOTR_ConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTR_ConverterApp.swift
+//  LOTR Converter
+//
+//  Created by Marcus Y. Kim on 4/5/23.
+//
+
+import SwiftUI
+
+@main
+struct LOTR_ConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
