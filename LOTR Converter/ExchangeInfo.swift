4fe355e76a1396cab12403a14f640ed0cf3bd917
Marcus Kim <marcuskim1989@gmail.com>
2023-04-08 13:06:52 -0700

    Preliminary layout for Exchange info

--- LOTR Converter/ExchangeInfo.swift
+++ LOTR Converter/ExchangeInfo.swift
@@ -25,18 +25,29 @@ struct ExchangeInfo: View {
                 // title text
                 
                 Text("Exchange Rates")
-                    .font(.largeTitle)
+                    .font(Font.custom("Hobbitonbrush", size: 50))
                 
                 //body text
+                
+                Text("Here at the Prancing Pony, we are happy to offer you a place where you can exchange all the known currencies in the entire world except one. We used to take Brandy Bucks, but after finding out that it was a person instead of a piece of paper, we realized it had no value to us. Below is a simple guide to our currency exchange rates:")
+                    .font(Font.custom("Hobbitonbrush", size: 20))
                 
                 // exchange rate {
                 HStack {
                     
                     // left currency image
-                    
+                    Image("goldpiece")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 33)
                     // exchange rater text
-                    
+                    Text("1 Gold Piece = 4 Gold Pennies")
+                        .font(Font.custom("Hobbitonbrush", size: 20))
                     // right currecny image
+                    Image("goldpenny")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(height: 33)
                 }
                 
                 //done button
